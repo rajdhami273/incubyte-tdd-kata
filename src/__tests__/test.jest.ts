e9fdcdb42d671969411cf4fd98fe25dbe5e66f24
morfysysdev-ss <morfysysdev.ss@smartdatainc.net>
2022-03-27 17:13:42 +0530

    Final push

--- src/__tests__/test.jest.ts
+++ src/__tests__/test.jest.ts
@@ -6,25 +6,22 @@ test("Should give answer 0 for empty or no string", () => {
 });
 
 test("Should give sum of all numbers separated by comma's", () => {
-  // const stringCalculator = new StringCalculator();
   expect(stringCalculator.Add("1,2,3,4,5,6")).toBe(21);
   expect(stringCalculator.Add("23,12")).toBe(35);
 });
 
 test("Should give sum of all numbers separated by comma's but including new line", () => {
-  // const stringCalculator = new StringCalculator();
   expect(stringCalculator.Add("1,2,\n3,4,\n5,6")).toBe(21);
   expect(stringCalculator.Add("\n23,\n12")).toBe(35);
 });
 
 test("Should give sum of all numbers separated by delimeter", () => {
-  // const stringCalculator = new StringCalculator();
   expect(stringCalculator.Add("//;\n1;2,\n3;4;\n5;6")).toBe(21);
   expect(stringCalculator.Add("//;\n1;2")).toBe(3);
+  expect(stringCalculator.Add("//]\n1]2")).toBe(3);
 });
 
-test("Should throw an error for negative number", () => {
-  // const stringCalculator = new StringCalculator();
+test("Should return false for negative number", () => {
   expect(stringCalculator.Add("//;\n1;2,\n3;-4;\n5;6")).toBe(false);
   expect(stringCalculator.Add("//;\n1;2")).toBe(3);
 });
