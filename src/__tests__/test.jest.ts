c3b09dfee3113f688d0b0714d90c849551ccbaaa
morfysysdev-ss <morfysysdev.ss@smartdatainc.net>
2022-03-27 16:55:44 +0530

    Added case of deliemeter

--- src/__tests__/test.jest.ts
+++ src/__tests__/test.jest.ts
@@ -1,7 +1,24 @@
 import StringCalculator from "../app/StringCalculator";
-
+const stringCalculator = new StringCalculator();
 test("Should give answer 0 for empty or no string", () => {
-  const stringCalculator = new StringCalculator();
   expect(stringCalculator.Add("")).toBe(0);
   expect(stringCalculator.Add()).toBe(0);
+});
+
+test("Should give sum of all numbers separated by comma's", () => {
+  // const stringCalculator = new StringCalculator();
+  expect(stringCalculator.Add("1,2,3,4,5,6")).toBe(21);
+  expect(stringCalculator.Add("23,12")).toBe(35);
+});
+
+test("Should give sum of all numbers separated by comma's but including new line", () => {
+  // const stringCalculator = new StringCalculator();
+  expect(stringCalculator.Add("1,2,\n3,4,\n5,6")).toBe(21);
+  expect(stringCalculator.Add("\n23,\n12")).toBe(35);
+});
+
+test("Should give sum of all numbers separated by delimeter", () => {
+  // const stringCalculator = new StringCalculator();
+  expect(stringCalculator.Add("//;\n1;2,\n3;4;\n5;6")).toBe(21);
+  expect(stringCalculator.Add("//;\n1;2")).toBe(3);
 });
--- src/app/StringCalculator.ts
+++ src/app/StringCalculator.ts
@@ -1,9 +1,33 @@
 export default class StringCalculator {
   constructor() {}
-  Add(numberString: string = ""): number {
+  Add(numberString: string = ""): number | boolean {
     if (numberString === "") {
       return 0;
     }
-    return 1;
+    const delimeter: string | boolean = this.isDelimeterPresent(numberString);
+    if (delimeter) {
+      numberString = this.ReplaceDelimeter(numberString, delimeter.toString());
+    }
+    const sanitizedNumberString = this.SanitizeString(numberString);
+    if (!sanitizedNumberString) {
+      return 0;
+    }
+    const numbers = sanitizedNumberString
+      .split(",")
+      .map((item) => Number(item || 0));
+    return numbers.reduce((acc, item) => (acc += item), 0);
+  }
+
+  isDelimeterPresent(numberString: string = ""): string | boolean {
+    if (numberString.substr(0, 2) === "//") {
+      return numberString[2];
+    }
+    return false;
+  }
+  ReplaceDelimeter(numberString: string = "", delimeter: string): string {
+    return numberString.substr(3).replace(RegExp(delimeter, "g"), ",");
+  }
+  SanitizeString(numberString: string = ""): string {
+    return numberString.replace(/\n/g, ",");
   }
 }
